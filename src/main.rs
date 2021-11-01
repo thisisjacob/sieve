acfd7e0bb8b962d269e713223c9ab4c9ab0824f4
thisisjacob <jacobholfelder@gmail.com>
2021-11-01 18:07:14 -0400

    Basic work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    if args.len() != 2 {
+        println!("Invalid arguments.");
+        std::process::exit(1);
+    }
+
+    // TODO: Check that limit > 2
+    let limit = args[1].parse::<u32>().expect("Not a valid limit.");
+    let nums = [2..limit + 1];
+    let p = 2;
 }
